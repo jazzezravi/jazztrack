5460cecb8a5bd0e5cef60cc7e0bb215ca836064b
Raveendran Perumalsamy <praveendran@athenahealth.com>
2017-07-03 14:13:25 +0530

    Admin Access Introduced

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615062708) do
+ActiveRecord::Schema.define(version: 20170703053919) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20170615062708) do
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "admin",            default: false
   end
 
 end
